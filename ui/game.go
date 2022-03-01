2a94262f0eae5508230ccdaa6f01b628483f3db6
Guilherme-De-Marchi <guilhermedemarchi13@gmail.com>
2022-03-01 17:34:01 -0300

    bugs fixed

--- ui/game.go
+++ ui/game.go
@@ -77,7 +77,7 @@ func (g *Game) UpdateWinGravityGrad() {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Clear()
-	screen.Fill(color.Black)
+	screen.Fill(color.White)
 
 	if g.EditOpt.ShowPauseScreen {
 		g.DrawPauseScreen(screen)
